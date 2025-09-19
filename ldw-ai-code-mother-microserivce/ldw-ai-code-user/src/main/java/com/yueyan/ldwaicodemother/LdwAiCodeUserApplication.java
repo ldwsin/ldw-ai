20b35079da9aa661aa0ea17c38e5521ca65c4b6d
ldws <18711554365@163.com>
2025-09-19 10:11:52 +0800

    (userfeat)微服务: 添加用户服务模块- 新增 LdwAiCodeUserApplication 启动类 - 添加用户服务相关的依赖 - 配置 MySQL、Redis 等 - 更新 CosClientConfig，增加条件注解

---
+++ ldw-ai-code-mother-microserivce/ldw-ai-code-user/src/main/java/com/yueyan/ldwaicodemother/LdwAiCodeUserApplication.java
@@ -0,0 +1,18 @@
+package com.yueyan.ldwaicodemother;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.yueyan.ldwaicodemother.mapper")
+@ComponentScan("com.yueyan")
+//@EnableDubbo
+public class LdwAiCodeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdwAiCodeUserApplication.class, args);
+    }
+}
